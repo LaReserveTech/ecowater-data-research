a763e589c0cbc3608da3e7ee4b9af80d29fe97af
grigaut <gaetanrigaut.s@gmail.com>
2023-03-02 22:25:29 +0100

    feat: loading and transforming functions

--- ecowater_data_research/loaders.py
+++ ecowater_data_research/loaders.py
@@ -1,10 +1,55 @@
-import datetime as dt
+import json
 
-import pandas as pd
+import geopandas as gpd
 import xarray as xr
 
-DATA_START_DATE = dt.datetime(1900, 1, 1)
+
+def load_from_cds(filepath: str) -> gpd.GeoDataFrame:
+    """Load GeoDataFrame from NetCDF file.
+
+    Parameters
+    ----------
+    filepath : str
+        Path to the NetCDF file containing the data.
+
+    Returns
+    -------
+    gpd.GeoDataFrame
+        GeoDataFrame from the file.
+    """
+    gdf = xr.open_dataset(filepath).to_dataframe().reset_index()
+    gdf["time"] = gdf["time"].dt.date
+    gdf.groupby(["latitude", "longitude", "time"]).sum().reset_index()
+    return gdf
 
 
-def load_from_cds(filepath: str) -> pd.DataFrame:
-    return xr.open_dataset(filepath).to_dataframe().reset_index()
+def load_geojson(filepath: str) -> dict:
+    """Load GeoJson
+
+    Parameters
+    ----------
+    filepath : str
+        Path to the geojson file.
+
+    Returns
+    -------
+    dict
+        Dictionnary corresponding to the geojson.
+    """
+    return json.load(open(filepath))
+
+
+def load_geotable(filepath: str) -> gpd.GeoDataFrame:
+    """Return transposed GeoDataFrame corresponding to the geojson.
+
+    Parameters
+    ----------
+    filepath : str
+        Path to the geojson file.
+
+    Returns
+    -------
+    gpd.GeoDataFrame
+        Transposed GeoDataFrame corresponding to the geojson.
+    """
+    return gpd.read_file(filepath).transpose()
